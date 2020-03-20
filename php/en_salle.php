e52d37c8cefc77e93a434bf8d1faa70ab996d1e7
FannyD-fantasy <dansmonbaluchon@gmail.com>
2020-03-20 16:04:10 +0100

    les tableaux en php

--- php/en_salle.php
+++ php/en_salle.php
@@ -13,19 +13,37 @@
   <header>
     <h1>Cinéma Rodia</h1>
     <?php 
-   // Je demande à PHP de récupérer le contenu d'un autre fichier avec l'instruction include (https://www.php.net/manual/fr/function.include.php)
-    // Le chemin vers le fichier est relatif au fichier qui l'inclus (on démarre le chemin du fichier à inclure à partir du chemin du fichier qui l'inclut)
-    // Si le fichier n'existe pas, PHP délenchera une erreur de type Warning et continuera de traiter le fichier. Si require est utilisé à la place d'include, une erreur de type Fatal sera déclenchée et PHP ne continuera pas le traitement
-    // On ne peut inclure que des fichiers PHP
-    // Si des variables sont crées dans le fichier inclus, elles seront accessibles dans la suite du code
-    // On peut utiliser l'inclusion de fichier avec PHP pour éviter de nous répéter dans le code HTML. On parle de factorisation du code HTML.
-    // La factorisation du code permet de faciliter le maintenance de celui-ci. Dans le cas d'une navigation factorisée, on n'aura plus qu'à modifier le template si une nouvelle page est à ajouter dans la navigation au lieu de modifier toutes les pages une par une.
     include 'template/navigation.php';
     ?>
   </header>
     <main> 
       <section>   
       <h2>En salle</h2>
+      <?php
+      // Je déclare une variable de type tableau (array)
+      // Un tableau est une liste
+      // Chaque valeur dans le tableau est séparée par une virgule
+      // Dans un tableau, on peut mettre des données de n'importe quel type : int, float, boolean, string, ... array
+      // Les éléments dans un tableau ont une position : un index. Par défaut, l'index du premier élément dans le tableau est 0
+      $filmsEnSalle = [
+          'Titanic',
+          'Le dîner de cons',
+          'Contagion',
+          'Joker',
+          'Le jour d\'après',
+      ];
+
+      ?>
+      <ul>
+        <?php
+        // J'affiche la première valeur contenu dans mon tableau (la valeur associée à l'index 0)
+        ?>
+        <li><?php echo $filmsEnSalle[0]; ?></li>
+        <li><?php echo $filmsEnSalle[1]; ?></li>
+        <li><?php echo $filmsEnSalle[2]; ?></li>
+        <li><?php echo $filmsEnSalle[3]; ?></li>
+        <li><?php echo $filmsEnSalle[4]; ?></li>
+      </ul>
       </section>
     </main>
   <footer>
