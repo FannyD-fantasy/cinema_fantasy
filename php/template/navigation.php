35c853833d345d64bf8be9c0ea6958e409d124d6
FannyD-fantasy <dansmonbaluchon@gmail.com>
2020-03-20 15:44:57 +0100

    création de la page en salle

--- php/template/navigation.php
+++ php/template/navigation.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="#">En salle</a></li>
+        <li><a href="en_salle.php">En salle</a></li>
         <li><a href="tarifs.php">Tarifs</a></li>
         <li><a href="#">Nous contacter</a></li>
     </ul>
