5559caff5983897962d6e0e9de1f7c13bf06c9b0
FannyD-fantasy <dansmonbaluchon@gmail.com>
2020-03-23 10:49:28 +0100

    correction challenge, sans bonus

--- php/en_salle.php
+++ php/en_salle.php
@@ -64,13 +64,13 @@
             'Hadès',
             'Zeus'
           ];
-          $nombreDeRooms = count($rooms);
+         // $nombreDeRooms = count($rooms);
           ?>
             <ul>
             <?php
               for (
                   $index = 0; // On veut afficher les éléments du tableau à partir du premier. On démarre à l'index 0.
-                  $index < $nombreDeRooms; // On veut afficher tous éléments du tableau. Tant que mon index ne dépasse la longueur de mon tableau (sachant que le dernier élément du tableau a pour index : longueur du tableau - 1), on continue la boucle
+                  $index < count($rooms); // On veut afficher tous éléments du tableau. Tant que mon index ne dépasse la longueur de mon tableau (sachant que le dernier élément du tableau a pour index : longueur du tableau - 1), on continue la boucle
                   $index = $index + 1 // On oublie par d'incrémenter l'index à chaque fin d'itération de la boucle (sinon boucle infinie)
               ) {
                   // Je peux utiliser la valeur d'une variable pour récupérer un élément de mon tableau. Attention, si ma variable ne correspond à aucun élément de mon tableau, PHP déclenchera une erreur.
