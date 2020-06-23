dc316548df9a3f364c60a06d80ce0250527f12dc
FannyD-fantasy <dansmonbaluchon@gmail.com>
2020-06-23 16:13:44 +0200

    avancement

--- php/index.php
+++ php/index.php
@@ -5,31 +5,84 @@
     $prices = getPrices();
     ?>
     <section class="presentation">
-      <div>
+      <div class="text_presentation">
         <h2>Découvrez notre cinéma familliale et atypique!</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur a excepturi quisquam, nesciunt consequuntur sunt soluta itaque quibusdam culpa distinctio tempora? Fugiat natus harum aspernatur, quibusdam sint soluta adipisci molestias.</p>
       </div>
-      <div>
-        <img src="" alt="">
+      <div class="img_presentation">
+        <img src="../assets/images/salle.webp" alt="">
       </div>
     </section>
-    <section class="movies">
+    <section id="ensalle" class="movies">
       <div>
         <h2>Films à l'affiche cette semaine :</h2>
       </div>
-      <div>
-        <img src="" alt="">
+
+      <div class="all_posters_movies">
+
+        <div class="line_posters_movies">
+          <div class="line_img_movies">
+            <div class="one_movie">
+              <img src="<?php echo $movies[0]['image']; ?>" alt="">
+            </div>
+            <div class="one_movie">
+              <img src="<?php echo $movies[0]['image']; ?>" alt="">
+            </div>
+            <div class="one_movie">
+              <img src="<?php echo $movies[0]['image']; ?>" alt="">
+            </div>
+            <div class="one_movie">
+              <img src="<?php echo $movies[0]['image']; ?>" alt="">
+            </div>
+            <div class="one_movie">
+              <img src="<?php echo $movies[0]['image']; ?>" alt="">
+            </div>
+          </div>
+          <div class="line_info-movies">
+            <div class="info_movie">
+              <div class="background">
+
+              </div>
+              <div class="button">
+
+              </div>
+              <div class="content_movie">
+                <div class="poster_movie">
+                  <img src="" alt="">
+                </div>
+                <div class="detail_movie">
+                  <h3>
+                    titre
+                    <span>avis</span>
+                  </h3>
+                  <h4>realisateur</h4>
+                  <h4>acteurs</h4>
+                  <div class="">
+                    <span>durée</span>
+                    <span>classification</span>
+                  </div>
+                  <p>synopsis</p>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
       </div>
-      <div>
-      </div>
+
+    
     </section>
-    <section class="film_session">
+    <section id="programme" class="film_session">
       <div>
         <h2>Programme - Semaine du 6 au 12 Juillet</h2>
       </div>
       <div class="day"></div>
     </section>
-    <section class="prices">
+    <section id="prices" class="prices">
       <div>
         <h2>Tarifs</h2>
       </div>
@@ -45,7 +98,7 @@
         </ul>
       </div>
     </section>
-    <section class="contact">
+    <section id="contact" class="contact">
       <div class="form_contact">
         <h2>Nous contacter</h2>
         <p>Une question ? une siggestion de film ? Ecrivez-nous un message:</p>
--- php/template/head.php
+++ php/template/head.php
@@ -2,27 +2,29 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Cinéma Fantasy -<?php echo $pageTitle; ?></title>
+  <title>Cinéma Fantasy</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
   <link rel="stylesheet" href="../assets/css/reset.css">
   <link rel="stylesheet" href="../assets/css/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;700&display=swap" rel="stylesheet">
   
 </head>
 <body>
   <header>
     <div class="header_container">
-      <div class="header_title">
-        <h1>Cinéma Fantasy</h1>
+      <div class="header_menu">
+        <div class="header_title">
+          <h1>Cinéma Fantasy</h1>
+        </div>
+        <nav class="header_nav">
+          <ul class="nav">
+            <li><a href="#ensalle">En salle</a></li>
+            <li><a href="#programme">Programme</a></li>
+            <li><a href="#prices">Tarifs</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
       </div>
-      <nav class="header_nav">
-        <ul>
-          <li>En salle</li>
-          <li>Programme</li>
-          <li>Tarifs</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
     </div>
-    
   </header>
   <main> 
