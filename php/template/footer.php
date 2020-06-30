73648339f141cad9c0ce76ad56c83753fb6f10e4
FannyD-fantasy <dansmonbaluchon@gmail.com>
2020-06-30 15:54:29 +0200

    calendar to finish

--- php/template/footer.php
+++ php/template/footer.php
@@ -2,7 +2,7 @@
   <footer>
     <section class="principal_footer">
       <div class="social_media">
-        <p>Suivez nous sur les reseaux sociaux</p>
+        <p class="footer_sociaux_text">Suivez nous sur les reseaux sociaux</p>
         <div class="media_sociaux_icone">
           <a href="#"><i class="fab fa-facebook"></i></a>
           <a href="#"><i class="fab fa-twitter-square"></i></i></a>
@@ -10,7 +10,49 @@
         </div>
       </div>
       <div class="date_time">
-        
+      <?php  /*************************  CALQUE CALENDRIER   ***************************************************/  ?>
+        <div id="LayerCalendar" >
+        <?php $mois=array(1=>"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"); ?>
+            <table width="250" border="0" cellpadding="0" cellspacing="0">
+            <tr height="30">
+          <td colspan="7"><div align="center" class="calendrier_mois"><?php echo $mois[date("n")]; ?> 2020</div></td>
+            </tr>  
+            <tr height="30">
+          <td colspan="7"><hr noshade size="1"></td>
+            </tr>
+            <tr height="30" class="calendrier_nom_des_jours">
+          <td>D</td><td>L</td><td>M</td><td>M</td><td>J</td><td>V</td><td>S</td>
+            </tr>   		  
+            <tr height="30">
+            <?php /* trouver le premier jour de la semaine */
+              $todayis = date("j", mktime(0, 0, 0, date("m"), date("d"), date("Y")));
+                $day = str_replace ('0', '7', date("w", mktime(0, 0, 0, date("m"), 1, date("Y")))); $col=0;
+                    $daynum = 1;
+              $daypermonth = date("j", mktime(0, 0, 0, date("m")+1, 0, date("Y")));
+              $nextdate = 0;
+              while($col<$day) {
+                  printf("<td><div align=\"center\" class=\"calendrier_jour\"></div></td>\n");
+                  $col++;
+                }
+              while($daynum<=$daypermonth) {
+                if ($col>6) {
+                    $col = 0;
+                  echo "</tr>";	/* fin de la ligne précédente */
+                  echo "<tr height='30'>"; /* nouvelle ligne */
+                }
+                                  /* affichage du jour */	
+                printf("<td><div align=\"center\" class=\"calendrier_jour\">");
+                      if($daynum == $todayis) printf("<b>%d</b>", $daynum); else printf("%d", $daynum);
+                      
+                                  printf("</div></td>\n");
+                
+                $daynum++;
+                $col++;
+              }
+                echo "<td colspan=".(7-$col)."></td></tr>";
+              ?>	
+          </table>
+        </div>
       </div>
       <div class="next_movies">
        <span class="next_movies_title">Découvrez les films à l'affiche la semaine prochaine:</span>
