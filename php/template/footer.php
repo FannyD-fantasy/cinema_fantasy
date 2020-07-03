84db90f0591877a4cd36bdb2ace7cf528c00de39
FannyD-fantasy <dansmonbaluchon@gmail.com>
2020-07-03 16:02:34 +0200

    calendar ok

--- php/template/footer.php
+++ php/template/footer.php
@@ -10,49 +10,72 @@
         </div>
       </div>
       <div class="date_time">
-      <?php  /*************************  CALQUE CALENDRIER   ***************************************************/  ?>
-        <div id="LayerCalendar" >
-        <?php $mois=array(1=>"Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"); ?>
-            <table width="250" border="0" cellpadding="0" cellspacing="0">
-            <tr height="30">
-          <td colspan="7"><div align="center" class="calendrier_mois"><?php echo $mois[date("n")]; ?> 2020</div></td>
-            </tr>  
-            <tr height="30">
-          <td colspan="7"><hr noshade size="1"></td>
-            </tr>
-            <tr height="30" class="calendrier_nom_des_jours">
-          <td>D</td><td>L</td><td>M</td><td>M</td><td>J</td><td>V</td><td>S</td>
-            </tr>   		  
-            <tr height="30">
-            <?php /* trouver le premier jour de la semaine */
-              $todayis = date("j", mktime(0, 0, 0, date("m"), date("d"), date("Y")));
-                $day = str_replace ('0', '7', date("w", mktime(0, 0, 0, date("m"), 1, date("Y")))); $col=0;
-                    $daynum = 1;
-              $daypermonth = date("j", mktime(0, 0, 0, date("m")+1, 0, date("Y")));
-              $nextdate = 0;
-              while($col<$day) {
-                  printf("<td><div align=\"center\" class=\"calendrier_jour\"></div></td>\n");
-                  $col++;
-                }
-              while($daynum<=$daypermonth) {
-                if ($col>6) {
-                    $col = 0;
-                  echo "</tr>";	/* fin de la ligne précédente */
-                  echo "<tr height='30'>"; /* nouvelle ligne */
-                }
-                                  /* affichage du jour */	
-                printf("<td><div align=\"center\" class=\"calendrier_jour\">");
-                      if($daynum == $todayis) printf("<b>%d</b>", $daynum); else printf("%d", $daynum);
-                      
-                                  printf("</div></td>\n");
-                
-                $daynum++;
-                $col++;
-              }
-                echo "<td colspan=".(7-$col)."></td></tr>";
-              ?>	
-          </table>
-        </div>
+      <style type="text/css">
+        #calendrier li { list-style: none; }
+        #libelle {width: 210px;margin: 0 auto;padding-bottom: 10px; list-style: none;}
+        .ligne {width: 210px;margin: 0 auto;padding: 0; list-style: none; } 
+        #libelle li {float : left;width : 30px; list-style: none; padding-bottom: 10px;}
+        .ligne li {color: white;float : left; width : 30px; list-style: none; padding-bottom: 10px;}
+        .ligne li a:hover{list-style: none;text-decoration: none; list-style: none;}
+        li.itemCurrentItem { color:#99681C;  }
+        li.itemSelectedItem { color:red; padding-top: 0; }
+      </style>
+      <?php
+        // fonctions utiles, $valeur représente une date au format AAAA-MM-JJ
+        function getDay($valeur) { return substr($valeur, 8, 2); }
+        function getMonth($valeur) { return substr($valeur, 5, 2); }
+        function getYear($valeur) { return substr($valeur, 0, 4); }
+        function monthNumToName($mois) {
+          $tableau = Array("", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aôut", "Septembre", "Octobre", "Novembre", "Décembre");
+          return (intval($mois) > 0 && intval($mois) < 13) ? $tableau[intval($mois)] : "Indéfini"; }
+        function showCalendar($periode) {
+          $leCalendrier = "";
+          // Tableau des valeurs possibles pour un numéro de jour dans la semaine
+          $tableau = Array("0", "1", "2", "3", "4", "5", "6", "0");
+          $nb_jour = date("t", mktime(0, 0, 0, getMonth($periode), 1, getYear($periode)));
+          $pas = 0;
+          $indexe = 1;
+          // Affichage du mois et de l'année
+          $leCalendrier .= "\n\t<h5><center>" . monthNumToName(getMonth($periode)) . " " . getYear($periode) . "</center></h2>";
+          // Affichage des entêtes
+          $leCalendrier .= "
+          <ul id=\"libelle\">
+            <b>\t<li>Lu</li>
+            \t<li>Ma</li>
+            \t<li>Me</li>
+            \t<li>Je</li>
+            \t<li>Ve</li>
+            \t<li>Sa</li>
+            \t<li>Di</li></b>
+          </ul>";
+          // Tant que l'on n'a pas affecté tous les jours du mois traité
+          while ($pas < $nb_jour) {
+            if ($indexe == 1) $leCalendrier .= "\n\t<ul class=\"ligne\">";
+            // Si le jour calendrier == jour de la semaine en cours
+            if (date("w", mktime(0, 0, 0, getMonth($periode), 1 + $pas, getYear($periode))) == $tableau[$indexe]) {
+              // Si jour calendrier == aujourd'hui
+              $afficheJour = sprintf("%02d",date("j", mktime(0, 0, 0, getMonth($periode), 1 + $pas, getYear($periode))));
+              if (date("Y-m-d", mktime(0, 0, 0, getMonth($periode), 1 + $pas, getYear($periode))) == date("Y-m-d")) $class = " class=\"itemCurrentItem\"";
+              else {
+                // 1 est toujours vrai => on affiche un lien à chaque fois
+                if (1) {
+                  $class = " class=\"\"";
+                  $afficheJour = sprintf("%02d",date("j", mktime(0, 0, 0, getMonth($periode), 1 + $pas, getYear($periode)))); }
+                else $class = ""; }
+              // Ajout de la case avec la date
+              $leCalendrier .= "\n\t\t<li$class>$afficheJour</li>";
+              $pas++; }
+            else {
+              // Ajout d'une case vide
+              $leCalendrier .= "\n\t\t<li> </li>"; }
+            if ($indexe == 7 && $pas < $nb_jour) { $leCalendrier .= "\n\t</ul>"; $indexe = 1;} else {$indexe++;} }
+          // Ajustement du tableau
+          for ($i = $indexe; $i <= 7; $i++) $leCalendrier .= "\n\t\t<li> </li>";
+          $leCalendrier .= "\n\t</ul>\n";
+          // Retour de la chaine contenant le Calendrier
+          return $leCalendrier; }
+        echo showCalendar(date("Y-m"));
+      ?>  
       </div>
       <div class="next_movies">
        <span class="next_movies_title">Découvrez les films à l'affiche la semaine prochaine:</span>
